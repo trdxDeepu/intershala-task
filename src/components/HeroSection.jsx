d546bdbb45a23867e0e1b453f49d140847c451fe
trdxlucifer <singhdepu566@gmail.com>
2023-08-01 15:24:17 +0530

    added images of company and there information

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,39 +1,38 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 
 const HeroSection = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [companyNames, setCompanyNames] = useState([
-    'Flipkart',
-    'Amazon',
-    'Google',
-    'Apple',
-    'media.not',
-    // Add more company names here
-  ]);
+  const companyData = [
+    {
+      name: 'Flipkart',
+      description: 'E-commerce company',
+      jobOpenings: ['Software Engineer', 'Product Manager'],
+      imageUrl: 'src/assets/flipkart.jpg',
+    },
+    {
+      name: 'Amazon',
+      description: 'E-commerce and cloud computing company',
+      jobOpenings: ['Software Engineer', 'Data Scientist'],
+      imageUrl: 'src/assets/amazon.png',
+    },
+    {
+      name: 'Google',
+      description: ' Cloud computing and Search Analytic',
+      jobOpenings: ['Software Engineer', 'Data Scientist'],
+      imageUrl: 'src/assets/google.png',
+    },
+    {
+      name: 'Twitter',
+      description: 'Social media Platform',
+      jobOpenings: ['Software Engineer', 'Data Scientist'],
+      imageUrl: 'src/assets/twitter.png',
+    },
+    // Add more company data objects here
+  ];
 
-  const handleSearchQueryChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
+  const [selectedCompany, setSelectedCompany] = useState(null);
 
-  const handleSearch = () => {
-    if (searchQuery.trim() === '') {
-      // If the search query is empty, reset the company names to the original list
-      setCompanyNames([
-        'Flipkart',
-        'Amazon',
-        'Google',
-        'Apple',
-        'media.not',
-        // Add more company names here
-      ]);
-    } else {
-      // Filter company names based on the search query
-      const filteredCompanies = companyNames.filter((name) =>
-        name.toLowerCase().includes(searchQuery.toLowerCase())
-      );
-      // Update the list of company names based on the search result
-      setCompanyNames(filteredCompanies);
-    }
+  const handleCompanyClick = (company) => {
+    setSelectedCompany(company);
   };
 
   return (
@@ -42,21 +41,34 @@ const HeroSection = () => {
         <div className="container">
           <h1>Find Your Dream Job</h1>
           <p>Join thousands of companies and job seekers who trust our platform.</p>
-          <div className="search-bar">
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={handleSearchQueryChange}
-          placeholder="Search for a job..."
-        />
-        <button className="search-button" onClick={handleSearch}>
-          Search
-        </button>
-       
-      </div>
-    
+          <div className="company-list">
+            {companyData.map((company, index) => (
+              <div key={index} onClick={() => handleCompanyClick(company)}>
+                <div className="company-item">
+                  <img src={company.imageUrl} alt={company.name} />
+                  <span>{company.name}</span>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </section>
+
+      {selectedCompany && (
+        <section className="company-details">
+          <div className="container">
+            <h2>{selectedCompany.name}</h2>
+            <p>{selectedCompany.description}</p>
+            <h3>Job Openings:</h3>
+            <ul>
+              {selectedCompany.jobOpenings.map((job, index) => (
+                <li key={index}>{job}</li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      )}
+
       <section className="job-categories">
     <div className="container">
       <h2>Job Categories</h2>
